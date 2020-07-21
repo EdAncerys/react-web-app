d8f9c07e6646b28cdd11ff2e7333c5e47b928bec
Ed <lookatemail@gmail.com>
2020-07-21 16:01:13 +0100

    Add elements and styling to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div>
-      <h1>Functional Footer Components</h1>
+    <div className="footer">
+      <React.Fragment>
+        <a>&copy; Copyright 2020. All Rights Reserved</a>
+      </React.Fragment>
+      <React.Fragment>
+        <a href="/">Hire Me</a>
+      </React.Fragment>
+      <React.Fragment>
+        <a href="/">Terms & Conditions</a>
+      </React.Fragment>
     </div>
   );
 }
