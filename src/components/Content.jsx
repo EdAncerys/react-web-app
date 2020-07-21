9d739ff337d56922ae7ee9c8bfc0f9d5b465a4ef
Ed <lookatemail@gmail.com>
2020-07-21 17:23:29 +0100

    Add feature banner

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,10 +3,16 @@ import React, { Component } from 'react';
 export default class Content extends Component {
   render() {
     return (
-      <div className="container">
-        <h1>Page Content</h1>
-        <h1>Page Content</h1>
-      </div>
+      <React.Fragment>
+        <div className="landing-banner">
+          <div className="selection-banner">Selected Projects</div>
+          <div className="content-banner">Main Content</div>
+        </div>
+        <div className="feature-banner">
+          <div className="feature-img">Feature img</div>
+          <div className="feature-content">Feature Content</div>
+        </div>
+      </React.Fragment>
     );
   }
 }
