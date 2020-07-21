b5616fac29b47eb0aea2392df280877ca433da0c
Ed <lookatemail@gmail.com>
2020-07-21 17:32:53 +0100

    Add year to render through the state dinamiclly

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Footer() {
-  return (
-    <div className="footer">
-      <React.Fragment>
-        <a>&copy; Copyright 2020. All Rights Reserved</a>
-      </React.Fragment>
-      <React.Fragment>
-        <a href="/">Hire Me</a>
-      </React.Fragment>
-      <React.Fragment>
-        <a href="/">Terms & Conditions</a>
-      </React.Fragment>
-    </div>
-  );
+export default class Footer extends Component {
+  state = {
+    year: new Date().getFullYear(),
+  };
+
+  render() {
+    return (
+      <div className="footer">
+        <React.Fragment>
+          <a>&copy; Copyright {this.state.year}. All Rights Reserved</a>
+        </React.Fragment>
+        <React.Fragment>
+          <a href="/">Hire Me</a>
+        </React.Fragment>
+        <React.Fragment>
+          <a href="/">Terms & Conditions</a>
+        </React.Fragment>
+      </div>
+    );
+  }
 }
