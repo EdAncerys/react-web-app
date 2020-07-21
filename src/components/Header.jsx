a7dcd2a4ce43ad574203f126e40c6a947a4c2b6a
Ed <lookatemail@gmail.com>
2020-07-21 11:18:29 +0100

    Add css styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { Navbar, Container } from 'react-bootstrap';
 
 export default class Header extends Component {
   render() {
     return (
       <div>
-        <h1>Header</h1>
+        <h1>NavBar</h1>
       </div>
     );
   }
