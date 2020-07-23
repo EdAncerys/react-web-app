3e72d0b48476aef94d9fe4ed5ea9100582f76d81
Ed <lookatemail@gmail.com>
2020-07-23 14:16:50 +0100

    Add About page content and css

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -3,10 +3,29 @@ import React, { Component } from 'react';
 export default class About extends Component {
   render() {
     return (
-      <React.Fragment>
+      <div className="about-page">
         <h1>About Me Page</h1>
-        <p>content</p>
-      </React.Fragment>
+        <p>
+          Driven and ambitious Software Developer with collaborative skills and
+          strong attention to detail, looking for opportunities to continue to
+          learn, challenge myself, and work with great people.
+        </p>
+        <p>
+          I' am a strong believer in the use of agile methodologies that helps
+          to drive and manage team projects. Enjoying challenges and don't shy
+          away from time-consuming challenges that can deliver better results
+          and continuous learning process it involves.
+        </p>
+        <p>
+          Recent{' '}
+          <a target="blank" href="https://makers.tech/">
+            Makers Academy
+          </a>{' '}
+          graduate where I have grown as a person and as a developer. Learned
+          best practices and methodologies: TDD, Agile development, XP values,
+          team projects, code reviews, pair programming.
+        </p>
+      </div>
     );
   }
 }
--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 export default class Contact extends Component {
   render() {
     return (
-      <React.Fragment>
+      <div className="contact-page">
         <h1>Get In Touch</h1>
         <p>Are you interested in working with me or just have a question?</p>
         <p>
           Email me at <strong>lookatemail@gmail.com</strong>
         </p>
-      </React.Fragment>
+      </div>
     );
   }
 }
