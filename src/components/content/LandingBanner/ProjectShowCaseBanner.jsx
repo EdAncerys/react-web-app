8747fb5bff6039658f3c2ddb5c99c846a855db03
Ed <lookatemail@gmail.com>
2020-07-24 16:09:09 +0100

    Add Airport Challenge section to landing banner

--- src/components/content/LandingBanner/ProjectShowCaseBanner.jsx
+++ src/components/content/LandingBanner/ProjectShowCaseBanner.jsx
@@ -105,6 +105,40 @@ export default class ProjectShowCaseBanner extends Component {
             </p>
           </div>
         )}
+        {this.props.selectedProjectId === 5 && (
+          <div className="content-banner">
+            <h4 className="titleName">{this.showCaseProject()}</h4>
+            <p>
+              We have a request from a client to write the software to control
+              the flow of planes at an airport. The planes can land and take off
+              provided that the weather is sunny. Occasionally it may be stormy,
+              in which case no planes can land or take off. Here are the user
+              stories that we worked out in collaboration with the client:
+            </p>
+            <p className="align-left">User Stories:</p>
+            <p>
+              As an air traffic controller, So I can get passengers to a
+              destination, I want to instruct a plane to land at an airport
+            </p>
+            <p>
+              As an air traffic controller, So I can get passengers on the way
+              to their destination, I want to instruct a plane to take off from
+              an airport and confirm that it is no longer in the airport
+            </p>
+            <p>
+              As the system designer So that the software can be used for many
+              different airports I would like a default airport capacity that
+              can be overridden as appropriate
+            </p>
+            <p>
+              Task is to test drive the creation of a set of classes/modules to
+              satisfy all the user stories. Need to use a random number
+              generator to set the weather (it is normally sunny but on rare
+              occasions it may be stormy). In tests, I use a stub to override
+              random weather to ensure consistent test behavior.
+            </p>
+          </div>
+        )}
       </React.Fragment>
     );
   }
