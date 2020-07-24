643efe9fe4569d14ef084ff78e338a1fac01bf29
Ed <lookatemail@gmail.com>
2020-07-24 19:28:10 +0100

    Add feature banner img

--- src/components/content/FeatureBanner/FeatureBanner.jsx
+++ src/components/content/FeatureBanner/FeatureBanner.jsx
@@ -4,7 +4,12 @@ export default class FeatureBanner extends Component {
   render() {
     return (
       <div className="feature-banner">
-        <div className="feature-img">Feature img</div>
+        <div className="feature-img-container">
+          <img
+            className="feature-img"
+            src={require('../../../images/map.png')}
+          />
+        </div>
         <div className="feature-content">Feature Content</div>
       </div>
     );
