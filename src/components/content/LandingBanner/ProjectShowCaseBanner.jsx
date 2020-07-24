e71a09e689868562ea5e8b899220bc4bb89ece73
Ed <lookatemail@gmail.com>
2020-07-24 14:49:27 +0100

    Add ThereAndBack section to landing banner

--- src/components/content/LandingBanner/ProjectShowCaseBanner.jsx
+++ src/components/content/LandingBanner/ProjectShowCaseBanner.jsx
@@ -17,10 +17,10 @@ export default class ProjectShowCaseBanner extends Component {
           <div className="content-banner">
             <h4 className="titleName">{this.showCaseProject()}</h4>
             <p>
-              I've detailed some of my favorite projects, things I'm proud of,
-              and all the skills I've developed since beginning to code. Here
-              you will find few highlights I worked on my own or as outlined by
-              the{' '}
+              I've detailed here some of my favorite projects, things I'm proud
+              of, and all the skills I've developed since beginning to code.
+              Here you will find a few highlights I worked on my own or as
+              outlined by the{' '}
               <a target="blank" href="https://makers.tech/">
                 Makers Academy
               </a>{' '}
@@ -31,7 +31,7 @@ export default class ProjectShowCaseBanner extends Component {
             <p>
               I'm always learning and evolving, faster than I can update my
               website, so the best place to ﬁnd out what I'm currently working
-              on is my GitHub portfolio.
+              on - is my GitHub portfolio.
             </p>
             <a target="blank" href="https://github.com/EdAncerys">
               GitHub portfolio
@@ -39,6 +39,25 @@ export default class ProjectShowCaseBanner extends Component {
             <a target="blank" href="https://www.linkedin.com/in/ancerys/">
               LinkedIn
             </a>
+          </div>
+        )}
+        {this.props.selectedProjectId === 2 && (
+          <div className="content-banner">
+            <h4 className="titleName">{this.showCaseProject()}</h4>
+            <p>
+              The final project created by team JAZE (Jed Stanton, Andrew Hulme,
+              Zsofia Szonyi, Ed Ancerys) at Makers Academy.
+            </p>
+            <p>
+              The app aims to solve the problem of the user who wants to find a
+              circular route for outdoor activities (walking, cycling, hiking).
+              The user can view the map and search for routes without logging
+              in, either circular routes or routes between a start and end
+              point. Different activity types can be selected, and the result
+              can be randomized. Once the user is logged in, favorite routes can
+              be saved in My Routes and can be viewed on the map, or they can be
+              removed from the list.
+            </p>
           </div>
         )}
       </React.Fragment>
