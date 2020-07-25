5b3d82968dfc7605660daa9fc0a82b343faf1e1d
Ed <lookatemail@gmail.com>
2020-07-25 14:07:02 +0100

    Add medium functional component

--- src/App.js
+++ src/App.js
@@ -72,6 +72,7 @@ export default function App() {
         )}
         {aboutPage && <About />}
         {contactPage && <Contact />}
+        {/* {mediumPage && <Medium />} */}
       </div>
       <Footer goToContactPage={goToContactPage} />
     </div>
---
+++ src/components/pages/Medium.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Medium() {
+  return (
+    <div>
+      <h1>Medium Articles</h1>
+    </div>
+  );
+}
