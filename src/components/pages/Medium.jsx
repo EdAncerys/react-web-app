b16290479770c98aad27140b7df293790d172ac7
Ed <lookatemail@gmail.com>
2020-07-25 14:51:02 +0100

    Istall axios

--- src/components/pages/Medium.jsx
+++ src/components/pages/Medium.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Medium() {
+  useEffect(() => console.log('mounted'));
+
   return (
     <div>
       <h1>Medium Articles</h1>
