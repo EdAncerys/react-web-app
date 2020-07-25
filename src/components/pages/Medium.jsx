8240d9539f2e84168b019f383ac52d8dae0ea979
Ed <lookatemail@gmail.com>
2020-07-25 15:43:22 +0100

    Able to catch errors with axios

--- src/components/pages/Medium.jsx
+++ src/components/pages/Medium.jsx
@@ -1,11 +1,39 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function Medium() {
-  useEffect(() => console.log('mounted'));
+  const [mediumArticles, setMediumArticles] = useState();
+  const [axiosError, setAxiosError] = useState();
+
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then(({ data }) => {
+        console.log(data);
+        setMediumArticles(data);
+      })
+      .catch(handleErrors);
+  }, []);
+
+  function handleErrors(err) {
+    if (err.response) {
+      const error = 2;
+      setAxiosError('Problem With Response ' + err.response.status);
+      console.log('Problem With Response ', err.response.status);
+    } else if (err.request) {
+      setAxiosError('Problem With Request!');
+      console.log('Problem With Request!');
+    } else {
+      setAxiosError('Error' + err.message);
+      console.log('Error', err.message);
+    }
+    console.log(axiosError, 4);
+  }
 
   return (
     <div>
       <h1>Medium Articles</h1>
+      {axiosError}
     </div>
   );
 }
