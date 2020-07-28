7cef92b7c69ecc02e71721d3f4c0625d62cacf53
Ed <lookatemail@gmail.com>
2020-07-28 10:37:25 +0100

    Able to handle state on a click event

--- src/components/pages/TickTackToe.jsx
+++ src/components/pages/TickTackToe.jsx
@@ -1,29 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function TickTackToe() {
-  function tileClicked(e) {
-    console.log('blah:', e.target.id);
+  const [tileClicked, setTileClicked] = useState(false);
+
+  function handleTileClicked(e) {
+    setTileClicked(!tileClicked);
+    console.log('clicked ', tileClicked, 'ID:', e.target.id);
   }
 
   return (
     <div className="tick-tack-toe-page">
       <h1>TickTackToe</h1>
       <div className="tick-tack-toe-board">
-        <div
-          id="1"
-          className="board-tile"
-          onClick={(e) => tileClicked(e)}
-        ></div>
-        <div
-          id="2"
-          className="board-tile"
-          onClick={(e) => tileClicked(e)}
-        ></div>
-        <div
-          id="3"
-          className="board-tile"
-          onClick={(e) => tileClicked(e)}
-        ></div>
+        <div className="tile-row">
+          <div
+            id="1"
+            className="board-tile"
+            onClick={(e) => handleTileClicked(e)}
+          ></div>
+          <div
+            id="2"
+            className="board-tile"
+            onClick={(e) => handleTileClicked(e)}
+          ></div>
+          <div
+            id="3"
+            className="board-tile"
+            onClick={(e) => handleTileClicked(e)}
+          ></div>
+        </div>
       </div>
     </div>
   );
