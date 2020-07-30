489164a1d2e0c055e67099d25deadc65c71fcdab
Ed <lookatemail@gmail.com>
2020-07-30 14:28:23 +0100

    Able to filter player one choice

--- src/components/content/TicTacToe/PlayerChoice.jsx
+++ src/components/content/TicTacToe/PlayerChoice.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 
 export default function PlayerChoice(props) {
-  const players = ['Peter', 'Lois', 'Glen', 'Brian', 'Neil', 'Charter'];
-  const player = props.playerOneTurn ? 'Player One' : 'Player Two';
+  const tickTackToeFigures = [
+    'Peter',
+    'Lois',
+    'Glen',
+    'Brian',
+    'Neil',
+    'Charter',
+  ];
+  const playerMove = props.playerOneTurn ? 'Player One' : 'Player Two';
+  const playerOneChoice = 'Peter1';
+
+  const tickTackToeFiguresLeft = tickTackToeFigures.filter((figure) => {
+    return figure !== playerOneChoice;
+  });
+
   return (
     <React.Fragment>
-      <p>{player} Pick Your Character</p>
+      <p>{playerMove} Pick Your Character</p>
       <div className="player-choice-container">
-        {players.filter((player) => {
-          return player !== 'Peter';
+        {tickTackToeFiguresLeft.map((figures) => {
+          return <p>{figures}</p>;
         })}
       </div>
     </React.Fragment>
