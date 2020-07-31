4295a6ffa632d9e498da783a9b8ed7a6066eb1bb
Ed <lookatemail@gmail.com>
2020-07-31 09:33:34 +0100

    Add family guy img to the project

--- src/components/content/TicTacToe/PlayerChoice.jsx
+++ src/components/content/TicTacToe/PlayerChoice.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import Peter from '../../../images/map-min.png';
+import PeterOne from '../../../images/TickTackToe/FamilyGuyCharacters/Peter-01-min.png';
+import PeterTwo from '../../../images/TickTackToe/FamilyGuyCharacters/Peter-02-min.png';
+import Brian from '../../../images/TickTackToe/FamilyGuyCharacters/Brian-min.png';
+import Chris from '../../../images/TickTackToe/FamilyGuyCharacters/Chris-min.png';
+import Glenn from '../../../images/TickTackToe/FamilyGuyCharacters/Glenn-min.png';
+import Herbert from '../../../images/TickTackToe/FamilyGuyCharacters/Herbert-min.png';
+import Joe from '../../../images/TickTackToe/FamilyGuyCharacters/Joe-min.png';
+import Lois from '../../../images/TickTackToe/FamilyGuyCharacters/Meg-min.png';
+import Stewie from '../../../images/TickTackToe/FamilyGuyCharacters/Stewie-min.png';
 
 export default function PlayerChoice(props) {
   const tickTackToeFigures = [
