9259a9be9f8a1b2dbc1447e5486c1353ea2862f3
Ed <lookatemail@gmail.com>
2020-08-04 12:36:32 +0100

    Add Nav bar buttons to dropdown menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,42 +18,31 @@ export default class Header extends Component {
           >
             {this.props.aboutPage ? 'Home' : 'About'}
           </Button>
-          <Button
-            onClick={this.props.goToContactPage}
-            className="btn btn-blue"
-            variant="light"
-          >
-            {this.props.contactPage ? 'Home' : 'Contact'}
-          </Button>
-          <Button
-            onClick={this.props.goToMediumPage}
-            className="btn btn-black"
-            variant="light"
-          >
-            {this.props.mediumPage ? 'Home' : 'Medium'}
-          </Button>
-          <Button
-            onClick={this.props.goToTickTackToePage}
-            className="btn btn-lemon"
-            variant="light"
-          >
-            {this.props.tickTackToePage ? 'Home' : 'TickTackToe'}
-          </Button>
-        </div>
-        <NavDropdown title="More" id="nav-dropdown">
-          <NavDropdown.Item onClick={this.props.goToMediumPage}>
-            {this.props.mediumPage ? 'Home' : 'Medium'}
-          </NavDropdown.Item>
-          <NavDropdown.Item onClick={this.props.goToTickTackToePage}>
-            {' '}
-            {this.props.tickTackToePage ? 'Home' : 'TickTackToe'}
-          </NavDropdown.Item>
 
-          <NavDropdown.Divider />
-          <NavDropdown.Item onClick={this.props.goToContactPage}>
-            {this.props.contactPage ? 'Home' : 'Contact'}
-          </NavDropdown.Item>
-        </NavDropdown>
+          <NavDropdown title="More" className="nav-dropdown">
+            <NavDropdown.Item
+              className="btn-nav-black"
+              onClick={this.props.goToMediumPage}
+            >
+              {this.props.mediumPage ? 'Home' : 'Medium'}
+            </NavDropdown.Item>
+            <NavDropdown.Item
+              className="btn-nav-lemon"
+              onClick={this.props.goToTickTackToePage}
+            >
+              {' '}
+              {this.props.tickTackToePage ? 'Home' : 'TickTackToe'}
+            </NavDropdown.Item>
+
+            <NavDropdown.Divider />
+            <NavDropdown.Item
+              className="btn-nav-blue"
+              onClick={this.props.goToContactPage}
+            >
+              {this.props.contactPage ? 'Home' : 'Contact'}
+            </NavDropdown.Item>
+          </NavDropdown>
+        </div>
       </Navbar>
     );
   }
