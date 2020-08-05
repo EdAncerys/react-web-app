668017ec0b40700e6c1a2ee04b0535114954058d
Ed <lookatemail@gmail.com>
2020-08-05 21:54:46 +0100

    Able to pass id onClick event

--- src/components/pages/RPS.jsx
+++ src/components/pages/RPS.jsx
@@ -2,13 +2,33 @@ import React from 'react';
 import Peter from '../../images/TickTackToe/TileBoards/Peter-min.png';
 
 export default function RPS() {
+  const handleRPSSelection = (e) => {
+    const id = e.target.id;
+    console.log(id);
+  };
+
   return (
     <div className="rps-page-component">
       <h1>Welcome to RPS</h1>
       <div className="rps-user-selection">
-        <img id="rock" src={Peter} className="rps-img"></img>
-        <img id="paper" src={Peter} className="rps-img"></img>
-        <img id="scissors" src={Peter} className="rps-img"></img>
+        <img
+          id="rock"
+          src={Peter}
+          className="rps-img"
+          onClick={(e) => handleRPSSelection(e)}
+        ></img>
+        <img
+          id="paper"
+          src={Peter}
+          className="rps-img"
+          onClick={(e) => handleRPSSelection(e)}
+        ></img>
+        <img
+          id="scissors"
+          src={Peter}
+          className="rps-img"
+          onClick={(e) => handleRPSSelection(e)}
+        ></img>
       </div>
     </div>
   );
