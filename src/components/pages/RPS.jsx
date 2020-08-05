d9248a1e688f0ca82b59b0370a9851fc103cdf82
Ed <lookatemail@gmail.com>
2020-08-05 21:40:31 +0100

    Add RPS selection images

--- src/components/pages/RPS.jsx
+++ src/components/pages/RPS.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Peter from '../../images/TickTackToe/TileBoards/Peter-min.png';
 
 export default function RPS() {
   return (
     <div className="rps-page-component">
       <h1>Welcome to RPS</h1>
+      <div className="rps-user-selection">
+        <img src={Peter}></img>
+        <img src={Peter}></img>
+        <img src={Peter}></img>
+      </div>
     </div>
   );
 }
