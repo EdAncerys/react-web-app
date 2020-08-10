893fb622b54af41289179935b53d26f3d8d7ec25
Ed <lookatemail@gmail.com>
2020-08-10 11:50:25 +0100

    Add onMause over & leave events

--- src/components/pages/HangMan.jsx
+++ src/components/pages/HangMan.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function HangMan() {
+  const [onHover, setOnHover] = useState(false);
+
+  const toggleHover = () => {
+    setOnHover(!onHover);
+    console.log('hello');
+  };
+
   return (
-    <div>
-      <h1>This is a Hang Man Game Page</h1>
+    <div style={styles.page}>
+      <h1
+        style={!onHover ? styles.mainText : styles.textOnHover}
+        onMouseEnter={toggleHover}
+        onMouseLeave={toggleHover}
+      >
+        This is a Hang Man Game Page
+      </h1>
     </div>
   );
 }
+
+const styles = {
+  page: {
+    minHeight: '80vh',
+    width: '100vw',
+    backgroundColor: 'gray',
+  },
+  mainText: {
+    color: '#fff',
+  },
+  textOnHover: {
+    color: 'tomato',
+  },
+};
