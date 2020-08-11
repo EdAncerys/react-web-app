8125bbf5ee5d961124101816b5b7c7bc071a2daf
Ed <lookatemail@gmail.com>
2020-08-11 17:00:19 +0100

    Add wrong letter separation

--- src/components/content/HangManComponent/WrongLetters.jsx
+++ src/components/content/HangManComponent/WrongLetters.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
 export default function WrongLetters({ wrongLetters, winner, game }) {
+  const handleWrongLetters = (wrongLetters) => {
+    return wrongLetters
+      .map((letter, i) => <span key={i}>{letter}</span>)
+      .reduce(
+        (prev, curr) => (prev === null ? [curr] : [prev, ', ', curr]),
+        null
+      );
+  };
+
   return (
     <div style={styles.container}>
       <p style={styles.mainText}>Wrong Letters Guessed</p>
       {!winner && (
         <React.Fragment>
           <p style={styles.mainText}>
-            {!wrongLetters.length > 0 ? 'Looking Good...' : wrongLetters}
+            {!wrongLetters.length > 0
+              ? 'Looking Good...'
+              : handleWrongLetters(wrongLetters)}
           </p>
           {wrongLetters.length > 0 && (
             <p style={{ ...styles.mainText, ...styles.whiteText }}>
