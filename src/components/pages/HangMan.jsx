3f63a91654becab09c49890721e2dfae1f7d6bd4
Ed <lookatemail@gmail.com>
2020-08-12 11:31:11 +0100

    Able to save hangMan game to local storage

--- src/components/pages/HangMan.jsx
+++ src/components/pages/HangMan.jsx
@@ -19,6 +19,45 @@ export default function HangMan() {
   const selectedWord1 = gameWords[Math.floor(Math.random() * gameWords.length)];
   const selectedWord = 'hello';
 
+  // Save state to local storage
+  const LOCAL_STORAGE_KEY = 'EdAncerys.HangMan';
+  const timeSaved = new Date().getTime();
+
+  const saveToLocalStorage = {
+    timeSaved: timeSaved,
+    correctLetters: correctLetters,
+    wrongLetters: wrongLetters,
+    playable: playable,
+    winner: winner,
+    gameWins: gameWins,
+    gameLoose: gameLoose,
+  };
+
+  const validateLocalStorage = () => {
+    const hours = 1;
+    const savedToJSON = localStorage.getItem(LOCAL_STORAGE_KEY);
+    const savedTimeToJSON = JSON.parse(savedToJSON).timeSaved;
+    if (new Date().getTime() - savedTimeToJSON > hours * 60 * 60 * 1000)
+      return false;
+    else return true;
+  };
+
+  useEffect(() => {
+    const savedToJSON = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (savedToJSON != null && validateLocalStorage()) {
+      setCorrectLetters(JSON.parse(savedToJSON).correctLetters);
+      setWrongLetters(JSON.parse(savedToJSON).wrongLetters);
+      setPlayable(JSON.parse(savedToJSON).playable);
+      setWinner(JSON.parse(savedToJSON).winner);
+      setGameWins(JSON.parse(savedToJSON).gameWins);
+      setGameLoose(JSON.parse(savedToJSON).gameLoose);
+    } else localStorage.clear();
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(saveToLocalStorage));
+  }, [saveToLocalStorage]);
+
   const playNewGame = () => {
     playAgain();
     setGameWins(0);
