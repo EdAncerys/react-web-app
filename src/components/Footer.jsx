2baa49caec598d7062f5a777c8204d12d59f396d
Ed <lookatemail@gmail.com>
2020-08-27 20:54:11 +0100

    Refactor Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,50 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
 import LinkedIn from '../images/LI-In-Bug.png';
 import colors from '../config/colors';
 
-export default class Footer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      year: new Date().getFullYear(),
-      date:
-        new Date().getFullYear() +
-        '-' +
-        (new Date().getMonth() + 1) +
-        '-' +
-        new Date().getDate(),
-    };
-  }
+export default function Footer({ goToContactPage }) {
+  const year = new Date().getFullYear();
+  const date = `${
+    new Date().getFullYear() +
+    '-' +
+    (new Date().getMonth() + 1) +
+    '-' +
+    new Date().getDate()
+  }`;
 
-  render() {
-    return (
-      <div style={styles.container} className="footer">
-        <React.Fragment>
-          <p>&copy; Copyright {this.state.year}. All Rights Reserved</p>
-        </React.Fragment>
-        <React.Fragment>
-          <p onClick={this.props.goToContactPage} className="hire-me">
-            Hire Me
-          </p>
-          <img
-            className="linked-in-logo"
-            alt="LinkedIn"
-            src={LinkedIn}
-            onClick={() =>
-              window.open('https://www.linkedin.com/in/ancerys/', '_blank')
-            }
-          />
-        </React.Fragment>
-        <React.Fragment>
-          <p>{this.state.date}</p>
-        </React.Fragment>
-      </div>
-    );
-  }
+  return (
+    <div style={styles.container}>
+      <React.Fragment>
+        <p>&copy; Copyright {year}. All Rights Reserved</p>
+      </React.Fragment>
+      <React.Fragment>
+        <p onClick={goToContactPage} className="hire-me">
+          Hire Me
+        </p>
+        <img
+          className="linked-in-logo"
+          alt="LinkedIn"
+          src={LinkedIn}
+          onClick={() =>
+            window.open('https://www.linkedin.com/in/ancerys/', '_blank')
+          }
+        />
+      </React.Fragment>
+      <React.Fragment>
+        <p>{date}</p>
+      </React.Fragment>
+    </div>
+  );
 }
 
 const styles = {
   container: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '90vw',
     color: colors.medium,
   },
 };
