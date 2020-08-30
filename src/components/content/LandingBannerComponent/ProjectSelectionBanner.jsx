4c1c254886880c6b66ac92c50b72972c55a64ad0
Ed <lookatemail@gmail.com>
2020-08-30 13:53:05 +0100

    Refactor ProjectSelectionBaner to functiional component

--- src/components/content/LandingBannerComponent/ProjectSelectionBanner.jsx
+++ src/components/content/LandingBannerComponent/ProjectSelectionBanner.jsx
@@ -1,29 +1,6 @@
-// import React, { Component } from 'react';
-// import SelectedProject from './SelectedProject';
-
-// export default class SelectionBanner extends Component {
-//   render() {
-//     return (
-//       <div className="selection-banner">
-//         <div className="selection-title">
-//           <h4 className="title-name">Featured Projects</h4>
-//         </div>
-//         {this.props.projects.map((project) => (
-//           <SelectedProject
-//             key={project.id}
-//             project={project}
-//             selectedProject={this.props.selectedProject}
-//           />
-//         ))}
-//       </div>
-//     );
-//   }
-// }
-
 import React, { useContext } from 'react';
 import { AppContext } from '../../../App';
 import SelectedProject from './SelectedProject';
-import colors from '../../../config/colors';
 
 export default function ProjectSelectionBanner({}) {
   const { projects } = useContext(AppContext);
