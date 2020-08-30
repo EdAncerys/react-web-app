0544ddc5e6ef4e6e93b6ab5aa0bb0e726674c1d5
Ed <lookatemail@gmail.com>
2020-08-30 13:55:35 +0100

    Refactor LandingBanner to funtional component

--- src/components/content/LandingBannerComponent/LandingBanner.jsx
+++ src/components/content/LandingBannerComponent/LandingBanner.jsx
@@ -1,40 +1,3 @@
-// import React, { Component } from 'react';
-// import SelectionBanner from './ProjectSelectionBanner';
-// import ProjectShowCaseBanner from './ProjectShowCaseBanner';
-
-// export default class LandingBanner extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       projects: [
-//         { id: 1, name: 'About', link: 'link1' },
-//         { id: 2, name: 'ThereAndBack', link: 'link1' },
-//         { id: 3, name: 'AceBook', link: 'link1' },
-//         { id: 4, name: 'MakersBnB', link: 'link1' },
-//         { id: 5, name: 'Airport Challenge', link: 'link1' },
-//         { id: 6, name: 'Takeaway Challenge', link: 'link1' },
-//         { id: 7, name: 'Bank tech test', link: 'link1' },
-//         { id: 8, name: 'Gilded Rose Refactoring Kata', link: 'link1' },
-//       ],
-//     };
-//   }
-
-//   render() {
-//     return (
-//       <div className="landing-banner">
-//         <SelectionBanner
-//           projects={this.state.projects}
-//           // selectedProject={this.props.selectedProject}
-//         />
-//         <ProjectShowCaseBanner
-//           projects={this.state.projects}
-//           selectedProjectId={this.props.selectedProjectId}
-//         />
-//       </div>
-//     );
-//   }
-// }
-
 import React from 'react';
 import ProjectSelectionBanner from './ProjectSelectionBanner';
 import ProjectShowCaseBanner from './ProjectShowCaseBanner';
@@ -43,14 +6,8 @@ import colors from '../../../config/colors';
 export default function LandingBanner({ props }) {
   return (
     <div style={styles.container}>
-      <ProjectSelectionBanner
-      // projects={projects}
-      // selectedProject={this.props.selectedProject}
-      />
-      {/* <ProjectShowCaseBanner
-      // projects={this.state.projects}
-      // selectedProjectId={this.props.selectedProjectId}
-      /> */}
+      <ProjectSelectionBanner />
+      <ProjectShowCaseBanner />
     </div>
   );
 }
