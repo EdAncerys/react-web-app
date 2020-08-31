483a541439d0f4e96a593d6a4c142d765bde13d2
Ed <lookatemail@gmail.com>
2020-08-31 14:22:26 +0100

    Refactor FronPage component

--- src/components/content/FeatureBannerComponent/FeatureBanner.jsx
+++ src/components/content/FeatureBannerComponent/FeatureBanner.jsx
@@ -1,96 +1,61 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
+import FeaturedElement from './FeaturedElement';
 import ThereAndBackImg from '../../../images/map-min.png';
 import TickTackToeImg from '../../../images/tick-tack-toe-min.png';
-import HangMan from '../../../images/hang-man-min.png';
 import Rock from '../../../images/RPS/rock-min.png';
-import FeaturedElement from './FeaturedElement';
+import HangMan from '../../../images/hang-man-min.png';
+import { AppContext } from '../../../App';
+
 import colors from '../../../config/colors';
-import { HangManContext } from '../../pages/HangMan';
+import RPS from '../../pages/RPS';
 
-export default class FeatureBanner extends Component {
-  render() {
-    return (
-      <div className="feature-banner">
-        <FeaturedElement
-          src={HangMan}
-          text="hello world"
-          color={colors.yellow}
-        />
+const hangManContent =
+  'Hangman is a paper and pencil guessing game for two or more players. One player thinks of a word, phrase or sentence and the other(s) tries to guess it by suggesting letters within a certain number of guesses.';
 
-        <div
-          className="feature-banner-component feature-content-2"
-          onClick={this.props.goToHangManPage}
-        >
-          <div className="feature-img-container">
-            <img className="feature-img" alt="ThereAndBack" src={HangMan} />
-          </div>
-          <div className="feature-content">
-            <p>
-              Hangman is a paper and pencil guessing game for two or more
-              players. One player thinks of a word, phrase or sentence and the
-              other(s) tries to guess it by suggesting letters within a certain
-              number of guesses.
-            </p>
-          </div>
-        </div>
-        <div
-          className="feature-banner-component feature-banner-component-2"
-          onClick={this.props.goToRPSPage}
-        >
-          <div className="feature-content">
-            <p>
-              Rock paper scissors is a hand game usually played between two
-              people, in which each player simultaneously forms one of three
-              shapes with an outstretched hand. These shapes are "rock" (a
-              closed fist), "paper" (a flat hand), and "scissors" (a fist with
-              the index finger and middle finger extended, forming a V).{' '}
-            </p>
-          </div>
-          <div className="feature-img-container">
-            <img className="feature-img" alt="TicTacToe" src={Rock} />
-          </div>
-        </div>
-        <div
-          className="feature-banner-component feature-content-2"
-          onClick={() =>
-            window.open('https://thereandback.surge.sh/', '_blank')
-          }
-        >
-          <div className="feature-img-container">
-            <img
-              className="feature-img"
-              alt="ThereAndBack"
-              src={ThereAndBackImg}
-            />
-          </div>
-          <div className="feature-content">
-            <p>
-              Variety - is a spice of life! ThereAndBack is a Web App that lets
-              users browse the best round routes possible. If its two legs or
-              four weals - we got you covered! For running, cycling,
-              walking...well, what a heck, even driving if that's a preference!
-              You can pick a route of your preference. And if you don't like it
-              the first time - just hit randomize to generate a different one.{' '}
-            </p>
-          </div>
-        </div>
-        <div
-          className="feature-banner-component feature-banner-component-2"
-          onClick={this.props.goToTickTackToePage}
-        >
-          <div className="feature-content">
-            <p>
-              Tic-tac-toe with a twist! This game commonly is a paper-and-pencil
-              game for two players, X and O, who take turns marking the spaces
-              in a 3×3 grid. The player who succeeds in placing three of their
-              marks in a horizontal, vertical, or diagonal row is the winner.{' '}
-            </p>
-          </div>
-          <div className="feature-img-container">
-            <img className="feature-img" alt="TicTacToe" src={TickTackToeImg} />
-          </div>
-        </div>
-      </div>
-    );
-  }
+const rpsContent =
+  "Rock paper scissors is a hand game usually played between two people, in which each player simultaneously forms one of three shapes with an outstretched hand. These shapes are 'rock' (a closed fist), 'paper' (a flat hand), and 'scissors' (a fist with the index finger and middle finger extended, forming a V).";
+
+const thereAndBackComntent =
+  "Variety - is a spice of life! ThereAndBack is a Web App that lets users browse the best round routes possible. If its two legs or four weals - we got you covered! For running, cycling, walking...well, what a heck, even driving if that's a preference! You can pick a route of your preference. And if you don't like it the first time - just hit randomize to generate a different one.";
+
+const tickTackToeContent =
+  'Tic-tac-toe with a twist! This game commonly is a paper-and-pencil game for two players, X and O, who take turns marking the spaces in a 3×3 grid. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner.';
+
+export default function FeatureBanner({ props }) {
+  const { goToHangManPage, goToTickTackToePage, goToRPSPage } = useContext(
+    AppContext
+  );
+
+  return (
+    <div style={styles.container}>
+      <FeaturedElement
+        src={ThereAndBackImg}
+        text={thereAndBackComntent}
+        reverse={true}
+        onClick={() => window.open('https://thereandback.surge.sh/', '_blank')}
+      />
+      <FeaturedElement
+        src={HangMan}
+        text={hangManContent}
+        color={colors.medium}
+        onClick={goToHangManPage}
+      />
+      <FeaturedElement
+        src={Rock}
+        text={rpsContent}
+        reverse={true}
+        onClick={goToRPSPage}
+      />
+      <FeaturedElement
+        src={TickTackToeImg}
+        text={tickTackToeContent}
+        color={colors.secondary}
+        onClick={goToTickTackToePage}
+      />
+    </div>
+  );
 }
+
+const styles = {
+  container: {},
+};
--- src/components/content/FeatureBannerComponent/FeaturedElement.jsx
+++ src/components/content/FeatureBannerComponent/FeaturedElement.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
 import colors from '../../../config/colors';
 
-export default function FeaturedElement({ src, text = 'Default Text', color }) {
+export default function FeaturedElement({
+  src,
+  text = 'Default Text',
+  color,
+  reverse = false,
+  onClick,
+}) {
   return (
     <div
       style={{
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
+        margin: 20,
+        border: `1px solid ${color ? color : colors.light}`,
         borderRadius: 20,
+        // boxShadow: `0 6px 16px 0 ${color ? color : colors.light}, 0 3px 6px 0 ${
+        //   color ? color : colors.light
+        // }`,
         width: '80vw',
         maxWidth: 1000,
         backgroundColor: color,
         overflow: 'hidden',
+        cursor: 'pointer',
       }}
+      onClick={onClick}
     >
       <div
         style={{
+          order: reverse ? 1 : 0,
           margin: 20,
           flex: 1,
           fontWeight: 'bold',
